52c1f8035b13f2eb92a0a5331455dafc219b2d42
econvener <mail@econvener.org>
2013-12-31 11:01:29 +1100

    Dashboard: Added dynamic data

--- core/views.py
+++ core/views.py
@@ -36,7 +36,12 @@ def user_logout(request):
 def dashboard(request):
 	if not request.user.is_authenticated():
 		return HttpResponseRedirect(reverse('index'))
-	return render_to_response('dashboard.html', {'user': request.user}, RequestContext(request))
+	tasks = Task.objects.filter(owner=request.user, status='Incomplete').order_by('deadline')
+	task_headings = ('Description', 'Assigned to', 'Deadline',)
+	meetings = Meeting.objects.filter(owner=request.user).order_by('date')
+	next_meeting = meetings.last()
+	last_meeting = meetings.first()
+	return render_to_response('dashboard.html', {'user': request.user, 'last_meeting': last_meeting, 'next_meeting': next_meeting, 'tasks': tasks, 'task_headings': task_headings}, RequestContext(request))
 	
 
 def participant_list(request):
