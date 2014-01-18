7883e4aa15f9dfc0f9f034001deee15fec74acec
econvener <mail@econvener.org>
2014-01-19 00:32:35 +1100

    Agendas: Added Task review to Agenda PDF

--- core/pdfs.py
+++ core/pdfs.py
@@ -10,20 +10,17 @@ from reportlab.platypus import Paragraph, SimpleDocTemplate, Spacer, Table, Tabl
 
 from core.models import Account, Meeting, Task
 
-from core.utils import calculate_meeting_duration
+from core.utils import calculate_meeting_duration, find_preceding_meeting_date
 
 
 def create_pdf_agenda(request, meeting_id, **kwargs):
+	# Set up the HttpResponse
 	response = HttpResponse(content_type='application/pdf')
 	response['Content-Disposition'] = 'filename="AgendaForPrinting.pdf"'
 #	Replace line above with the line below after testing is finished
 #	response['Content-Disposition'] = 'attachment; filename="AgendaForPrinting.pdf"'
     
-	styles = getSampleStyleSheet()
-	normalStyle = styles['Normal']
-	heading1Style = styles['Heading1']
-	heading2Style = styles['Heading2']
-	
+    # Set up the document framework
 	buffer = BytesIO()
 	doc = SimpleDocTemplate(buffer,
 		rightMargin=20*mm,
@@ -32,12 +29,27 @@ def create_pdf_agenda(request, meeting_id, **kwargs):
 		bottomMargin=20*mm,
 		pagesize=A4)
 	Document = []
+    
+    # Define text styles
+	styles = getSampleStyleSheet()
+	normalStyle = styles['Normal']
+	heading1Style = styles['Heading1']
+	heading2Style = styles['Heading2']
+	heading3Style = styles['Heading3']
+		
+	# Define colors
 	heading_color = CMYKColor(0.4,0,0.2,0)	
 	background_color = CMYKColor(0.2,0,0.1,0)
-	
+			
+	# Generate the data which will populate the document
 	account = Account.objects.filter(owner=request.user).last()
 	meeting = Meeting.objects.get(pk=int(meeting_id))
 	preliminary_items = meeting.item_set.filter(owner=request.user, variety__exact='preliminary')
+	incomplete_task_list = Task.objects.filter(owner=request.user, status="Incomplete")
+	completed_task_list = []
+	preceding_meeting_date = find_preceding_meeting_date(request.user, meeting_id)
+	if preceding_meeting_date != None:
+		completed_task_list = Task.objects.filter(owner=request.user, status="Complete", deadline__gte=preceding_meeting_date).exclude(deadline__gte=meeting.date)
 	meeting_duration = calculate_meeting_duration(meeting_id)
 		
 	# Agenda heading
@@ -75,11 +87,39 @@ def create_pdf_agenda(request, meeting_id, **kwargs):
 		t.setStyle(TableStyle(
 			[('GRID', (0,0), (1,-1), 0.5, black),
 			('BACKGROUND', (0, 0), (-1, 0), background_color),
-			('LEFTPADDING', (0, 0), (-1, 0), 0),]))
+			('LEFTPADDING', (0, 0), (-1, 0), 0),
+			('TOPPADDING', (0, 0), (-1, 0), 0),
+			('BOTTOMPADDING', (0, 0), (-1, 0), 0),]))
 		Document.append(t)
 		Document.append(Spacer(0,3*mm))
 	
-		
+	# Task review - Tasks outstanding
+	Document.append(Paragraph("Task review", heading2Style))
+	Document.append(Paragraph("Tasks outstanding", heading3Style))
+	headings = ('Description', 'Assigned to', 'Deadline')
+	incomplete_tasks = [(task.description, task.participant, task.deadline) for task in incomplete_task_list]
+	t = Table([headings] + incomplete_tasks, colWidths=[280,100,70], hAlign='LEFT')
+	t.setStyle(TableStyle(
+		[('GRID', (0,0), (-1,-1), 0.5, black),
+		('BACKGROUND', (0, 0), (-1, 0), background_color)]))
+	Document.append(t)
+	Document.append(Spacer(0,3*mm))
+	
+	# Task review - Tasks completed since last meeting
+	Document.append(Paragraph("Tasks completed since last meeting", heading3Style))
+	completed_tasks = [(task.description, task.participant, task.deadline) for task in completed_task_list]
+	if completed_task_list:
+		t = Table([headings] + completed_tasks, colWidths=[280,100,70], hAlign='LEFT')
+	else:
+		t = Table([headings] + [('No tasks','','')], colWidths=[280,100,70], hAlign='LEFT')
+	t.setStyle(TableStyle(
+		[('GRID', (0,0), (-1,-1), 0.5, black),
+		('BACKGROUND', (0, 0), (-1, 0), background_color)]))
+	Document.append(t)
+	Document.append(Spacer(0,3*mm))
+	
+	
+	
 	# Build and return the PDF
 	doc.build(Document)
 	
