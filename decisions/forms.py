9d09a7812bea3eb6b4c728f9703a690a3010ed11
econvenor <mail@econvenor.org>
2014-03-12 22:56:44 +1100

    Got decisions working

--- decisions/forms.py
+++ decisions/forms.py
@@ -5,6 +5,17 @@ from decisions.models import Decision
 
 class MinutesDecisionForm(forms.ModelForm):
     
+    def __init__(self, group, *args, **kwargs):
+        super(MinutesDecisionForm, self).__init__(*args, **kwargs)
+
     class Meta:
         model = Decision
         
+        fields = ['description']
+        
+    def save(self, group, commit=True):
+        decision = super(MinutesDecisionForm, self).save(commit=False)
+        decision.group = group
+        if commit:
+            decision.save()
+        return decision
--- decisions/views.py
+++ decisions/views.py
@@ -3,24 +3,23 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from decisions.models import Decision
-from utilities.commonutils import get_current_group	
+from utilities.commonutils import get_current_group
 
-	
+
 def decision_list(request):
     group = get_current_group(request)
     if group == None:	
         return HttpResponseRedirect(reverse('index'))
-        
-	decisions = Decision.objects.filter(group=group)
 
-	page_heading = 'Decisions'
-	table_headings = ('Decision',
-	                  'Meeting',
-	                  'Agenda item',
-	                  )
+    decisions = Decision.objects.filter(group=group)
+    page_heading = 'Decisions'
+    table_headings = ('Decision',
+                      'Meeting',
+                      'Agenda item',
+                      )
 
-	return render(request, 'decision_list.html', {
-	              'decisions': decisions,
-	              'page_heading': page_heading,
-	              'table_headings': table_headings,
-	              })
+    return render(request, 'decision_list.html', {
+                  'decisions': decisions,
+                  'page_heading': page_heading,
+                  'table_headings': table_headings,
+                  })
