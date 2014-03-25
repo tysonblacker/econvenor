40f2376261f920aab549b5d54a1bf9c0cf4f4973
econvenor <mail@econvenor.org>
2014-03-25 15:02:31 +1100

    Set up stateful highlighting of navbar parent items

--- bugs/views.py
+++ bugs/views.py
@@ -7,119 +7,137 @@ from bugs.forms import BugForm, FeatureForm
 
 
 def bug_report(request):
-	if not request.user.is_authenticated():
-		return HttpResponseRedirect(reverse('index'))
-	page_heading = 'Report a bug'
-	if request.method == "POST":
-		bug_form = BugForm(request.POST)
-		if bug_form.is_valid():
-		    bug_form.save(request.user)
-		    return HttpResponseRedirect(reverse('bug-list'))
-	else:
-		bug_form = BugForm()
-	return render(request, 'bug_report.html', {
-	             'bug_form': bug_form,
-	             'page_heading': page_heading,
-	             })
-	
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('index'))
+    page_heading = 'Report a bug'
+    if request.method == "POST":
+        bug_form = BugForm(request.POST)
+        if bug_form.is_valid():
+            bug_form.save(request.user)
+            return HttpResponseRedirect(reverse('bug-list'))
+    else:
+        bug_form = BugForm()
+
+    menu = {'parent': 'feedback'}            	
+    return render(request, 'bug_report.html', {
+                  'menu': menu,
+                  'bug_form': bug_form,
+                  'page_heading': page_heading,
+                  })
+
 
 def bug_edit(request, bug_id):
-	if not request.user.is_authenticated():
-		return HttpResponseRedirect(reverse('index'))
-	page_heading = 'Bug report'
-	administrator = 1
-	allow_status_editing = False
-	bug_form = {}
-	bug = Bug.objects.get(pk=bug_id)
-	if (request.user.id == administrator) or (request.user.id == int(bug.owner_id)):
-		display_mode = 'edit'
-		if request.user.id == administrator:
-			allow_status_editing = True
-		if request.method == "POST":
-			bug_form = BugForm(request.POST, instance=bug)
-			if bug_form.is_valid():
-			    bug_form.save()
-			    return HttpResponseRedirect(reverse('bug-list'))
-		else:
-			bug_form = BugForm(instance=bug)
-	else:
-		display_mode = 'view'
-	return render(request, 'bug_edit.html', {
-	              'display_mode': display_mode,
-	              'bug': bug,
-	              'bug_form': bug_form,
-	              'page_heading': page_heading,
-	              'allow_status_editing': allow_status_editing,
-	              })
-	
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('index'))
+    page_heading = 'Bug report'
+    administrator = 1
+    allow_status_editing = False
+    bug_form = {}
+    bug = Bug.objects.get(pk=bug_id)
+    if (request.user.id == administrator) or (request.user.id == int(bug.owner_id)):
+        display_mode = 'edit'
+        if request.user.id == administrator:
+            allow_status_editing = True
+        if request.method == "POST":
+            bug_form = BugForm(request.POST, instance=bug)
+            if bug_form.is_valid():
+                bug_form.save()
+                return HttpResponseRedirect(reverse('bug-list'))
+        else:
+            bug_form = BugForm(instance=bug)
+    else:
+        display_mode = 'view'
+
+    menu = {'parent': 'feedback'}            	
+    return render(request, 'bug_edit.html', {
+                  'menu': menu,
+                  'display_mode': display_mode,
+                  'bug': bug,
+                  'bug_form': bug_form,
+                  'page_heading': page_heading,
+                  'allow_status_editing': allow_status_editing,
+                  })
+
 
 def bug_list(request):
-	if not request.user.is_authenticated():
-		return HttpResponseRedirect(reverse('index'))
-	bugs = Bug.objects.all()
-	page_heading = 'Bugs reported'
-	table_headings = ('Bug number', 'Description', 'Date reported', 'Status')
-	return render(request, 'bug_list.html', {
-	              'bugs': bugs,
-	              'page_heading': page_heading,
-	              'table_headings': table_headings,
-	              })
-	
-	
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('index'))
+    bugs = Bug.objects.all()
+    page_heading = 'Bugs reported'
+    table_headings = ('Bug number', 'Description', 'Date reported', 'Status')
+
+    menu = {'parent': 'feedback'}            	
+    return render(request, 'bug_list.html', {
+                  'menu': menu,
+                  'bugs': bugs,
+                  'page_heading': page_heading,
+                  'table_headings': table_headings,
+                  })
+
+
 def feature_request(request):
-	if not request.user.is_authenticated():
-		return HttpResponseRedirect(reverse('index'))
-	page_heading = 'Request a feature/change'
-	if request.method == "POST":
-		feature_form = FeatureForm(request.POST)
-		if feature_form.is_valid():
-		    feature_form.save(request.user)
-		return HttpResponseRedirect(reverse('feature-list'))
-	else:
-		feature_form = FeatureForm()
-	return render(request, 'feature_request.html', {
-	              'feature_form': feature_form,
-	              'page_heading': page_heading,
-	              })
-	
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('index'))
+    page_heading = 'Request a feature/change'
+    if request.method == "POST":
+        feature_form = FeatureForm(request.POST)
+        if feature_form.is_valid():
+            feature_form.save(request.user)
+        return HttpResponseRedirect(reverse('feature-list'))
+    else:
+        feature_form = FeatureForm()
+
+    menu = {'parent': 'feedback'}            	
+    return render(request, 'feature_request.html', {
+                  'menu': menu,
+                  'feature_form': feature_form,
+                  'page_heading': page_heading,
+                  })
+
 
 def feature_edit(request, feature_id):
-	if not request.user.is_authenticated():
-		return HttpResponseRedirect(reverse('index'))
-	page_heading = 'Feature/change request'
-	administrator = 1
-	allow_status_editing = False
-	feature_form = {}
-	feature = Feature.objects.get(pk=feature_id)
-	if (request.user.id == administrator) or (request.user.id == int(feature.owner_id)):
-		display_mode = 'edit'
-		if request.user.id == administrator:
-			allow_status_editing = True
-		if request.method == "POST":
-			save_and_add_owner(request, FeatureForm(request.POST, instance=feature))
-			return HttpResponseRedirect(reverse('feature-list'))
-		else:
-			feature_form = FeatureForm(instance=feature)
-	else:
-		display_mode = 'view'
-	return render(request, 'feature_edit.html', {
-	              'display_mode': display_mode,
-	              'feature': feature,
-	              'feature_form': feature_form,
-	              'page_heading': page_heading,
-	              'allow_status_editing': allow_status_editing,
-	              })
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('index'))
+    page_heading = 'Feature/change request'
+    administrator = 1
+    allow_status_editing = False
+    feature_form = {}
+    feature = Feature.objects.get(pk=feature_id)
+    if (request.user.id == administrator) or (request.user.id == int(feature.owner_id)):
+        display_mode = 'edit'
+        if request.user.id == administrator:
+            allow_status_editing = True
+        if request.method == "POST":
+            save_and_add_owner(request, FeatureForm(request.POST, instance=feature))
+            return HttpResponseRedirect(reverse('feature-list'))
+        else:
+            feature_form = FeatureForm(instance=feature)
+    else:
+        display_mode = 'view'
+
+    menu = {'parent': 'feedback'}            	
+    return render(request, 'feature_edit.html', {
+                  'menu': menu,
+                  'display_mode': display_mode,
+                  'feature': feature,
+                  'feature_form': feature_form,
+                  'page_heading': page_heading,
+                  'allow_status_editing': allow_status_editing,
+                  })
 
 
 def feature_list(request):
-	if not request.user.is_authenticated():
-		return HttpResponseRedirect(reverse('index'))
-	features = Feature.objects.all()
-	page_heading = 'Features/changes requested'
-	table_headings = ('Request number', 'Description', 'Date requested', 'Status',)
-	return render(request, 'feature_list.html', {
-	              'features': features,
-	              'page_heading': page_heading,
-	              'table_headings': table_headings,
-	              })
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('index'))
+    features = Feature.objects.all()
+    page_heading = 'Features/changes requested'
+    table_headings = ('Request number', 'Description', 'Date requested', 'Status',)
+
+    menu = {'parent': 'feedback'}            	
+    return render(request, 'feature_list.html', {
+                  'menu': menu,
+                  'features': features,
+                  'page_heading': page_heading,
+                  'table_headings': table_headings,
+                  })
 
--- help/views.py
+++ help/views.py
@@ -1,23 +1,40 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.shortcuts import render
 
 from help.utils import convert_markdown_to_html
 from utilities.commonutils import set_path
-	
-	
+
+
 def user_guide(request):
-	MARKDOWN_PATH = set_path('help/markdown/', '/home/econvenor/webapps/econvenor/econvenor/core/markdown/')
-	page_content = convert_markdown_to_html(MARKDOWN_PATH + 'user_guide.mkd')
-	return render_to_response('markdown_template.html', {'page_content': page_content}, RequestContext(request))
+    MARKDOWN_PATH = set_path('help/markdown/',
+        '/home/econvenor/webapps/econvenor/econvenor/core/markdown/')
+    page_content = convert_markdown_to_html(MARKDOWN_PATH + 'user_guide.mkd')
+
+    menu = {'parent': 'help'}            	
+    return render(request, 'markdown_template.html', {
+                  'menu': menu,
+                  'page_content': page_content,
+                  })
 
 
 def faqs(request):
-	MARKDOWN_PATH = set_path('help/markdown/', '/home/econvenor/webapps/econvenor/econvenor/core/markdown/')
-	page_content = convert_markdown_to_html(MARKDOWN_PATH + 'faqs.mkd')
-	return render_to_response('markdown_template.html', {'page_content': page_content}, RequestContext(request))
-    
+    MARKDOWN_PATH = set_path('help/markdown/',
+        '/home/econvenor/webapps/econvenor/econvenor/core/markdown/')
+    page_content = convert_markdown_to_html(MARKDOWN_PATH + 'faqs.mkd')
+
+    menu = {'parent': 'help'}            	
+    return render(request, 'markdown_template.html', {
+                  'menu': menu,
+                  'page_content': page_content
+                  })
+
 
 def ask_question(request):
-	MARKDOWN_PATH = set_path('help/markdown/', '/home/econvenor/webapps/econvenor/econvenor/core/markdown/')
-	page_content = convert_markdown_to_html(MARKDOWN_PATH + 'ask_question.mkd')
-	return render_to_response('markdown_template.html', {'page_content': page_content}, RequestContext(request))
+    MARKDOWN_PATH = set_path('help/markdown/',
+        '/home/econvenor/webapps/econvenor/econvenor/core/markdown/')
+    page_content = convert_markdown_to_html(MARKDOWN_PATH + 'ask_question.mkd')
+
+    menu = {'parent': 'help'}            	
+    return render(request, 'markdown_template.html', {
+                  'menu': menu,
+                  'page_content': page_content
+                  })
