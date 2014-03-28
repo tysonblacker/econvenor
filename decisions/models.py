84e7fb7975924cfab7771c7a71aaa0747f494c54
econvenor <mail@econvenor.org>
2014-03-28 13:19:32 +1100

    Added a status field to decisions model

--- decisions/models.py
+++ decisions/models.py
@@ -8,12 +8,22 @@ from meetings.models import Meeting
 
 class Decision(TimeStampedModel):
 
+    STATUS_CHOICES = (
+        ('Distributed', 'Distributed'),
+        ('Draft', 'Draft'),
+        )
+        
     group = models.ForeignKey(Group)
 
     item = models.ForeignKey(Item)
     meeting = models.ForeignKey(Meeting)
     
     description = models.TextField(null=False, blank=False)
+    status = models.CharField(max_length=20,
+                              choices=STATUS_CHOICES,
+                              default='Draft',
+                              null=False,
+                              blank=True)
     
     def __unicode__(self):
         return self.description
