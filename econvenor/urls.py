8a77447dd8a2c8cbbcf28866ee7306338f80451c
Sky <sky@econvenor.org>
2014-06-12 10:44:17 +1000

    Added a robots.txt file

--- econvenor/urls.py
+++ econvenor/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 admin.autodiscover()
 
@@ -92,6 +93,8 @@ urlpatterns = patterns('',
 		name="feature-request"),
 	url(r'^features/(\d{1,4})/edit/$', 'bugs.views.feature_edit',
 		name="feature-edit"),       
+	
+	url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 		
 )   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
