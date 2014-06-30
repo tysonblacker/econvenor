28e4083deca4bee499678743f0c60ab856247403
Sky <sky@econvenor.org>
2014-06-30 17:00:56 +1000

    Introduced 'ENVIRONMENT' variable to settings.py and wsgi.py

--- econvenor/wsgi.py
+++ econvenor/wsgi.py
@@ -14,9 +14,33 @@ import socket
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "econvenor.settings")
 
-HOST_NAME = os.environ['ECONVENOR_HOST_NAME']
+ENVIRONMENT = os.environ['ECONVENOR_ENVIRONMENT']
 
-if socket.gethostname() == HOST_NAME:
+if ENVIRONMENT == 'development':
+	from django.core.wsgi import get_wsgi_application
+	application = get_wsgi_application()
+elif ENVIRONMENT == 'test':
+	site.addsitedir(
+		'/home/econvenor/.virtualenvs/env_test/lib/python2.7/site-packages'
+	)
+	activate_this = os.path.expanduser(
+		"~/.virtualenvs/env_test/bin/activate_this.py"
+	)
+	execfile(activate_this, dict(__file__=activate_this))
+
+	# Calculate the path based on the location of the WSGI script
+	project = '/home/econvenor/webapps/econvenor_test/'
+	workspace = os.path.dirname(project)
+	sys.path.append(workspace)
+
+	sys.path = ['/home/econvenor/webapps/econvenor_test/econvenor',
+		'/home/econvenor/webapps/econvenor_test/any_otherPaths?',
+		'/home/econvenor/webapps/econvenor_test'
+	]+ sys.path
+
+	from django.core.handlers.wsgi import WSGIHandler
+	application = WSGIHandler()
+elif ENVIRONMENT == 'production':
 	site.addsitedir(
 		'/home/econvenor/.virtualenvs/env/lib/python2.7/site-packages'
 	)
@@ -37,6 +61,3 @@ if socket.gethostname() == HOST_NAME:
 
 	from django.core.handlers.wsgi import WSGIHandler
 	application = WSGIHandler()
-else:
-	from django.core.wsgi import get_wsgi_application
-	application = get_wsgi_application()
