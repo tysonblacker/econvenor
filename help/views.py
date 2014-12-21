0c7e76990655661b62758564628cb87012e0dc30
Sky <sky@econvenor.org>
2014-12-22 00:51:01 +1100

    Removed Markdown dependency

--- help/views.py
+++ help/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-from help.utils import convert_markdown_to_html
 from utilities.commonutils import set_path
 
 
@@ -29,12 +28,8 @@ def qanda(request):
 
 
 def ask_question(request):
-    MARKDOWN_PATH = set_path('help/markdown/',
-        '/home/econvenor/webapps/econvenor/econvenor/help/markdown/')
-    page_content = convert_markdown_to_html(MARKDOWN_PATH + 'ask_question.mkd')
 
     menu = {'parent': 'help'}            	
-    return render(request, 'markdown_template.html', {
+    return render(request, 'ask_question.html', {
                   'menu': menu,
-                  'page_content': page_content
                   })
